fec47442bfcc5574a4683500f047a543b0a78cc3
Dania Abujabal <dan20190691@std.psut.edu.jo>
2023-12-26 16:03:01 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/gpgp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gpgp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'Pages/Home_Page.dart';
+import 'Pages/Search_Page.dart';
+import 'Pages/Categories_Page.dart';
+import 'Pages/navigation.dart';
+
+void main()  {
+  WidgetsFlutterBinding.ensureInitialized();
+   Firebase.initializeApp();
+  runApp(const MyApp());
+}
+  class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+  }
+
+  class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+  return const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MainNavigationPage(),
+
+
+    );
+  }
+  }
+
+
+/*
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home:  HomePage(),
+    );
+  }
+}
+
+ */
+
